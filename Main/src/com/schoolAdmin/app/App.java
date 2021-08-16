c0ca1d5412d4aaa5045902b17b1ec472acc6a1ef
Sean Riley <rileyhawk249@gmail.com>
2021-08-17 00:34:29 +0200

    Added global key event for login button

--- Main/src/com/schoolAdmin/app/App.java
+++ Main/src/com/schoolAdmin/app/App.java
@@ -14,6 +14,8 @@ import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import java.sql.SQLException;
 
+import org.w3c.dom.events.MouseEvent;
+
 public class App extends Application {
 
     Postgres psql = new Postgres();
@@ -53,80 +55,75 @@ public class App extends Application {
         */
         
         //For users who like to use their mouse
-       final String user = userNameField.getText();
-       final String pass =passwordField.getText();
-       
-    //    private void authenticate(final Node keyNode){
-    //        final EventHandler<KeyEvent> submit = new EventHandler<KeyEvent>(){
-    //            public void enter_Key(final KeyEvent keyEvent){
-    //                if(keyEvent.getCode() == KeyCode.ENTER){
-    //                    (KeyEvent.getEventType() == KeyEvent.KEY_PRESSED);
 
-    //                    keyEvent.consume();
-    //                }
-    //            }
-    //        };
-
-    //        passwordField.setOnKeyPressed(submit);
-    //    }
-       
-    //    loginBtn.setOnAction(action -> {
-    //         // System.out.println(userNameField.getText() + " " +passwordField.getText());
-    //               Postgres.validate(user, pass);
-    //     });
-        
-        final EventHandler<KeyEvent> submit = new EventHandler<KeyEvent>(){
+        final EventHandler<KeyEvent> subKey = new EventHandler<KeyEvent>(){
             public void handle(KeyEvent e){
-                if(e.getCode().equals(KeyCode.ENTER)){
-            
-            
-                  try {
-                    psql.validate(user, pass);
+                final String user = userNameField.getText();
+                final String pass =passwordField.getText();
+                
+                if(e.getCode().equals(KeyCode.ENTER)){ 
+                    try {
+                        boolean val = psql.validate(user, pass);
+                        if(val){
+                            System.out.println("User Found");
+                            // e.consume();             
+                   } else{
+                       System.out.println("Didnt find user");
+                   }
                 } catch (SQLException e1) {
                     // TODO Auto-generated catch block
                     e1.printStackTrace();
                 }
-                  System.out.println("Retrieved text"); 
+                  //System.out.println("Retrieved text"); 
+                //It prints retrieved but after printing fail.
                 } else{
                     System.out.println("Failed");
                 }
             }
         };
+//  TODO: # Fix click mouse event for login button
+        // final EventHandler<InputEvent> subClick = new EventHandler<InputEvent>(){
+        //     public void handle(InputEvent e){
+        //         final String user = userNameField.getText();
+        //         final String pass =passwordField.getText();
+                
+        //         if(e.getEventType() .equals(MouseEvent.AT_TARGET)){ 
+        //             try {
+        //                 boolean val = psql.validate(user, pass);
+        //                 if(val){
+        //                     System.out.println("User Found");
+        //                     // e.consume();             
+        //            } else{
+        //                System.out.println("Didnt find user");
+        //            }
+        //         } catch (SQLException e1) {
+        //             // TODO Auto-generated catch block
+        //             e1.printStackTrace();
+        //         }
 
-        passwordField.setOnKeyPressed(submit);
 
-        //Gets Username and password when enter key is pressed on password field
-        // passwordField.setOnKeyPressed(new EventHandler<KeyEvent>() throws SQLException{
-        //     @Override
-        //     public void handle(KeyEvent e){
-        //         if(e.getCode().equals(KeyCode.ENTER)){
+        //         } else{
+        //             System.out.println("Failed");
+        //         }
+        //     }
+        // };
+        // loginBtn.addEventHandler(KeyEvent.KEY_PRESSED, subKey);
+
+        //  Find a way to handle mouse click events
+        // loginBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, subClick);
+
+
+
+
+            //global event listener for enter key to submit
+            TPane.addEventHandler(KeyEvent.KEY_PRESSED, subKey);
+
           
-        //      user=userNameField.getText();
-        //      pass=passwordField.getText(); 
-        //           Postgres.validate(user, pass);
-        //           System.out.println(user+pass);
-        // }      
-        //     }
-        // });
 
-        // public void login(ActionEvent event) throws SQLException{
-        //     if(usernameField.getText().isEmpty()){
 
-        //     } 
-        // }
-//TODO: Rewrite functions and clear clutter
 
-        // public void submitAction(ActionEvent submit) throws SQLException{
 
-        //     boolean flag = psql.validate(user, pass);
-    
-        //     if(!flag){
-        //         System.out.println("Failed to authenticate");
-        //     } else{
-        //         System.out.println("Authentication Successful");
-        //     }
-        // }
-        
+
 
         /**Tile Pane Prefs**/
         TPane.setPrefColumns(1);
