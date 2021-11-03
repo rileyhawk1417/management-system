fced48a4953defe548db52b41632ba7b8f2345f2
riley <rileyhawk249@gmail.com>
2021-11-03 17:07:27 +0200

    Added more cheats to manage screen navigation

--- Main/src/com/schoolAdmin/controllers/misc/SceneCtrl.java
+++ Main/src/com/schoolAdmin/controllers/misc/SceneCtrl.java
@@ -7,25 +7,24 @@ import java.io.IOException;
 
 import javafx.fxml.FXMLLoader;
 
-
 public class SceneCtrl {
-    //TODO not seeing resources folder
-  public  static String login_screen = "/resources/fxml/login_screen.fxml";
-   public static String add_screen = "/resources/fxml/insert_screen.fxml";
-   public static String records_screen = "/resources/fxml/records_view.fxml";
+    // TODO not seeing resources folder
+    public static String login_screen = "/resources/fxml/login_screen.fxml";
+    public static String add_screen = "/resources/fxml/admin/insert_screen.fxml";
+    public static String records_screen = "/resources/fxml/admin/records_view.fxml";
     public static String greeting_screen = "/resources/fxml/greeting_banner.fxml";
+    public static String update_screen = "/resources/fxml/admin/update_screen.fxml";
 
-
-    public static void switchScene(Scene scene, boolean truth, String title, boolean option){
+    public static void switchScene(Scene scene, boolean truth, String title, boolean option) {
         Stage stage = new Stage();
         stage.setScene(scene);
         stage.setResizable(truth);
         stage.setTitle(title);
-        if(option == true){
+        if (option == true) {
             stage.show();
-        } else if(option == false){
+        } else if (option == false) {
             stage.showAndWait();
-        } else{
+        } else {
             System.out.println("Invalid Option");
         }
 
@@ -33,36 +32,47 @@ public class SceneCtrl {
 
     }
 
-    public void login_scene(){
-        try { 
-        Parent root = FXMLLoader.load(getClass().getResource(login_screen));
-        Scene login = new Scene(root);
-        switchScene(login, true, "Login Screen", false);
-    } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
+    public void login_scene() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(login_screen));
+            Scene login = new Scene(root);
+            switchScene(login, true, "Login Screen", false);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void rec_scene() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(records_screen));
+            Scene records = new Scene(root);
+            switchScene(records, true, "View Inventory ", false);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void add_scene() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(add_screen));
+            Scene add = new Scene(root);
+            switchScene(add, true, "Add Record", false);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
     
-    public void rec_scene(){ 
-    try {
-        Parent root = FXMLLoader.load(getClass().getResource(records_screen));
-        Scene records = new Scene(root);
-        switchScene(records, true, "View Records ", false);
-    } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-        }
-    }
-    
-    public void add_scene(){
-    try {
-        Parent root = FXMLLoader.load(getClass().getResource(add_screen));
-        Scene add = new Scene(root);
-        switchScene(add, true, "Add Records", false);
-    } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
+    public void update_scene() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(update_screen));
+            Scene add = new Scene(root);
+            switchScene(add, true, "Update Record", false);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
