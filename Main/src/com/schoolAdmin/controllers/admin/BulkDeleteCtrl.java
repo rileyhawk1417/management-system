b1b337cdbbc19c65fa55670cafa65ae231389ac6
riley <rileyhawk249@gmail.com>
2021-11-04 22:09:08 +0200

    Bulk name delete controller

---
+++ Main/src/com/schoolAdmin/controllers/admin/BulkDeleteCtrl.java
@@ -0,0 +1,62 @@
+package com.schoolAdmin.controllers.admin;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+// import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Alert;
+import javafx.stage.Window;
+
+import com.schoolAdmin.database.Mysql;
+import com.schoolAdmin.modals.AlertModule;
+
+public class BulkDeleteCtrl {
+    Mysql mysql = new Mysql();
+    
+    @FXML
+    private Button cancel_;
+
+    @FXML
+    private Button bulk_delete;
+    
+    @FXML
+    private TextField search_;
+    
+    Window owner;
+    @FXML
+    private void cancel_delete(){
+    owner = search_.getScene().getWindow();
+        owner.hide();
+    }
+
+
+    @FXML
+    private void confirm_delete(){
+    owner = search_.getScene().getWindow();
+        // boolean result = 
+    try {
+        boolean result = confirm();
+        if(result){
+           //Only run if records got deleted
+            System.out.println("Records deleted");
+            AlertModule.showAlert(Alert.AlertType.INFORMATION, owner, "Records Deleted", "Records were successfully deleted");
+            owner.hide();
+        }
+    } catch (Exception e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }       
+        
+    }
+    
+    private boolean confirm() throws Exception{
+        if (search_.getText().isEmpty()){
+            //dont run
+            AlertModule.showAlert(Alert.AlertType.ERROR, owner, "Empty Fields", "No empty fields allowed!");
+            return false;
+        } else {
+            mysql.delete_row_by_name(search_.getText());
+            return true;
+        }
+    }
+}
