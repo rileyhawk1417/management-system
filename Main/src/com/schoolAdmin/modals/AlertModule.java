2ba318ae96b96994068eadc15c2a148b92fad9e2
riley <rileyhawk249@gmail.com>
2021-11-08 18:51:51 +0200

    Added Descriptive comments

--- Main/src/com/schoolAdmin/modals/AlertModule.java
+++ Main/src/com/schoolAdmin/modals/AlertModule.java
@@ -2,7 +2,9 @@ package com.schoolAdmin.modals;
 
 import javafx.scene.control.Alert;
 import javafx.stage.Window;
-
+/*
+* *This module is responsible for pop up messages
+*/
 public class AlertModule {
     public static void showAlert(Alert.AlertType alertType, Window owner, String title, String msg){
         Alert alert = new Alert(alertType);
